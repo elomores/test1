de04f05ca2aa17b13a7dc0e4774327bbdc7867e8
elomores <elomores@mail.ru>
2020-06-30 00:57:20 +0300

    Короче, реализации массива препятствий подспиздил, но немного под себя переделал. Посмотри плз

--- HW1/src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MoveThroughObstacles test = new MoveThroughObstacles();
-        test.initialize();
-    }
-}
